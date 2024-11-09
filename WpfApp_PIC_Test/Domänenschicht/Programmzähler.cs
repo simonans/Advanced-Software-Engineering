0ad87879f8b5573f2a065f7a50bf0188d1bfa000
Oiram321 <mario.grimm2000gmail.com>
2024-11-09 18:14:18 +0100

    merge conflict fixing

---
+++ WpfApp_PIC_Test/Domänenschicht/Programmzähler.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp_PIC.Domänenschicht___Datenmodel
+{
+    public class Programmzähler
+    {
+        private int _programcounter;
+
+        public Programmzähler()
+        {
+            _programcounter = 0;
+        }
+
+        public void ResetProgramCounter()
+        {
+            _programcounter = 0;
+        }
+
+        public void IncreaseProgramCounter()
+        {
+            _programcounter++;
+        }
+
+        public void SetProgrammCounter(int newValue)
+        {
+            _programcounter = newValue;
+        }
+    }
+}
