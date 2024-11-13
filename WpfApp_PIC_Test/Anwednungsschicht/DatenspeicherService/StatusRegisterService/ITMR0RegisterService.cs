7c05544fa0428f2acd66a5c27fb13492b82d89d5
simonans <anselm710@gmail.com>
2024-11-13 07:21:37 +0100

    Added TMR0Service

---
+++ WpfApp_PIC_Test/Anwednungsschicht/DatenspeicherService/StatusRegisterService/ITMR0RegisterService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp_PIC.Anwednungsschicht.DatenspeicherService.StatusRegisterService
+{
+    public interface ITMR0RegisterService
+    {
+        int GetValue();
+
+        void SetValue(int value);
+
+        void increaseValue();
+    }
+}
---
+++ WpfApp_PIC_Test/Anwednungsschicht/DatenspeicherService/StatusRegisterService/TMR0RegisterService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using WpfApp_PIC.Domänenschicht;
+
+namespace WpfApp_PIC.Anwednungsschicht.DatenspeicherService.StatusRegisterService
+{
+    class TMR0RegisterService : ITMR0RegisterService
+    {
+        private DataRegister _dataRegister;
+
+        public TMR0RegisterService(DataRegister dataRegister)
+        {
+            _dataRegister = dataRegister;
+        }
+
+        public int GetValue()
+        {
+            return _dataRegister.GetValue(0);
+        }
+
+        public void SetValue(int value)
+        {
+            _dataRegister.SetValue(1, value);
+        }
+
+        public void increaseValue()
+        {
+            _dataRegister.SetValue(1, GetValue() + 1);
+        }
+    }
+}
