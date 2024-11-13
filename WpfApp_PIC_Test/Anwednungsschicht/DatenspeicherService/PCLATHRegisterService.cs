8efb922513fc8806cc8795412e74912a015c8603
simonans <anselm710@gmail.com>
2024-11-13 07:37:08 +0100

    Added PCLATHService

---
+++ WpfApp_PIC_Test/Anwednungsschicht/DatenspeicherService/PCLATHRegisterService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfApp_PIC.Domänenschicht;
+
+namespace WpfApp_PIC.Anwednungsschicht.DatenspeicherService
+{
+    public class PCLATHRegisterService : IPCLATHRegisterService
+    {
+        private DataRegister _dataRegister;
+
+        public PCLATHRegisterService(DataRegister dataRegister)
+        {
+            _dataRegister = dataRegister;
+        }
+
+        public int GetValue()
+        {
+            return _dataRegister.GetValue(0);
+        }
+
+        public void SetValue(int value)
+        {
+            _dataRegister.SetValue(10, value);
+        }
+
+        public void increaseValue()
+        {
+            _dataRegister.SetValue(10, GetValue() + 1);
+        }
+    }
+}
